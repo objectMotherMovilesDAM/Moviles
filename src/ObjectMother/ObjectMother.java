96fb87120f9c3cd14f6eacf86a32fe7f24e25fc2
Albertux2 <61083022+Albertux2@users.noreply.github.com>
2021-12-17 12:09:17 +0100

    Calcular procesador+ram y generador de nombres

--- src/ObjectMother/ObjectMother.java
+++ src/ObjectMother/ObjectMother.java
@@ -1,7 +1,49 @@
 package ObjectMother;
 
-public class ObjectMother {
-    private String[] marcas = {"PocoPhone", "Xiaomi", "Apple", "Huawei", "Samsung", "Oppo", "Pixel", "Nokia", "Realme", "Sony", "LG", "Honor", "OnePlus", "Motorola", "HTC", "ZTE", "Asus"};
-    private String[] modelos = {"Note", "Edge", "Moto", "Pro", "MX", "Solar", "Mate", "Lite", "Versa", "GT", "GTR", "X", "BW", "Neo", "TWS","Mi","Red Magic","Nord","Reno","Master","Find"};
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class ObjectMother {
+    private final int MAX_NUCLEOS = 12;
+    Random random = new Random();
+
+    private String[] marcas = {"PocoPhone", "Xiaomi", "Apple", "Huawei", "Samsung", "Oppo", "Pixel", "Nokia", "Realme", "Sony", "LG", "Honor", "OnePlus", "Motorola", "HTC", "ZTE", "Asus"};
+    private String[] modelos = {"Note", "Edge", "Moto", "Pro", "MX", "Solar", "Mate", "Lite", "Versa", "GT", "GTR", "X", "BW", "Neo", "TWS", "Mi", "Red Magic", "Nord", "Reno", "Master", "Find"};
+
+
+    public ArrayList<Movil> generarMoviles(int cantidad){
+        ArrayList<Movil> moviles = new ArrayList<Movil>();
+        for (int i = 0; i < cantidad; i++) {
+
+        }
+        return moviles;
+    }
+
+
+    private int calcularMinimaRam(Procesador procesador) {
+        int medioGB = 512;
+        return procesador.getNucleos() * medioGB + random.nextInt(8)*medioGB;
+    }
+
+    private Procesador calcularProcesador() {
+        int nucleos = getNucleosRandom();
+        int frecuencia = calcularFrecuencia(nucleos);
+        Procesador procesador = new Procesador(nucleos,frecuencia,RandomNombre.crearPalabra(6));
+        return procesador;
+    }
+
+    private int getNucleosRandom(){
+        return random.nextInt(MAX_NUCLEOS);
+    }
+
+    private int calcularFrecuencia(int nucleos){
+        if(nucleos<=4) {
+            return nucleos*450;
+        }else if(nucleos<=8){
+            return nucleos*250;
+        }else{
+            return nucleos*180;
+        }
+    }
 }
---
+++ src/ObjectMother/RandomNombre.java
@@ -0,0 +1,29 @@
+package ObjectMother;
+
+import java.util.Random;
+
+public class RandomNombre {
+    private static Random random = new Random();
+
+
+    public static String crearPalabra(int longitudPalabra) {
+        int a = (int) 'a';
+        char vocales[] = {'a','e','i','o','u'};
+        String palabra = "";
+        boolean vocal = false;
+        for (int i = 0; i < longitudPalabra; i++) {
+            if(vocal){
+                palabra = palabra+(vocales[random.nextInt(vocales.length)]);
+            }else{
+                int letraNueva = (char) random.nextInt('z' - 'a' + 1);
+                palabra = palabra+(char)(letraNueva+a);
+            }
+            vocal = !vocal;
+        }
+        return palabra;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RandomNombre.crearPalabra(10));
+    }
+}
